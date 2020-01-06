4d43accfe49effc9ed472e9cbb0718e0e0d77a12
Hapaxia <MJSilk2@gmail.com>
2020-01-06 16:41:19 +0000

    update year
    updated year from 2019 to 2020

--- src/SelbaWard/ElasticSprite.cpp
+++ src/SelbaWard/ElasticSprite.cpp
@@ -5,7 +5,7 @@
 //
 // Elastic Sprite
 //
-// Copyright(c) 2017-2019 M.J.Silk
+// Copyright(c) 2017-2020 M.J.Silk
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
