b6aa36dc0157601948469a71ab5586980b8ebac8
Hapaxia <Hapaxia@users.noreply.github.com>
2024-01-03 12:58:40 +0000

    update year (2024)
    update year for 2024

--- src/SelbaWard/Polygon.hpp
+++ src/SelbaWard/Polygon.hpp
@@ -5,7 +5,7 @@
 //
 // Polygon
 //
-// Copyright(c) 2022-2023 M.J.Silk
+// Copyright(c) 2022-2024 M.J.Silk
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
